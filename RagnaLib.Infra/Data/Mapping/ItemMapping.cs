20ef52f6d9fd484f6d32b590f48b5ef47613e375
berthot <matheus.berthot@gmail.com>
2021-03-15 20:53:42 -0300

    item mapping

--- RagnaLib.Infra/Data/Mapping/ItemMapping.cs
+++ RagnaLib.Infra/Data/Mapping/ItemMapping.cs
@@ -1,7 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RagnaLib.Domain.Entities;
+
 namespace RagnaLib.Infra.Data.Mapping
 {
     public class ItemMapping
     {
-        
+        public ItemMapping(EntityTypeBuilder<Item> entity)
+        {
+            entity.HasKey(x => x.Id)
+                .HasName("PK_ITEM");
+            entity.ToTable("Item");
+
+            entity.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            entity.Property(x => x.Name)
+                .IsRequired();
+
+            entity.Property(x => x.Price);
+
+            entity.Property(x => x.ImageUrl);
+
+            entity.Property(x => x.SmallImageUrl);
+
+            entity.Property(x => x.CardImageUrl);
+
+            entity.Property(x => x.Description);
+            
+        }
     }
+    // public ItemType ItemTypeId { get; set; }
+    // public ItemType ItemType { get; set; }
 }
