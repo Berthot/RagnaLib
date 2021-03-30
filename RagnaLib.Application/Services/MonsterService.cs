bd32a2e20d3085e63b9b092ea360c6be0ba7d1e7
Matheus Bertho <matheus.berthot@gmail.com>
2021-03-30 11:26:35 -0300

    insert IMonsterRepository in service

--- RagnaLib.Application/Services/MonsterService.cs
+++ RagnaLib.Application/Services/MonsterService.cs
@@ -1,18 +1,22 @@
 using RagnaLib.Domain.Bases.Abstracts;
 using RagnaLib.Domain.Bases.Interfaces;
+using RagnaLib.Domain.Repositories;
 using RagnaLib.Infra.Repositories;
 
 namespace RagnaLib.Application.Services
 {
-    public class MonsterService : ServiceBase<MonsterRepository>
+    public class MonsterService : ServiceBase<IMonsterRepository>
     {
-        public MonsterService(MonsterRepository repo) : base(repo)
+        public MonsterService(IMonsterRepository repo) : base(repo)
         {
         }
-
+        
+        
         public void Test()
         {
             var x = Repo.GetAll(); // esta certo?
         }
+
+
     }
 }
--- RagnaLib.Infra/Repositories/MonsterRepository.cs
+++ RagnaLib.Infra/Repositories/MonsterRepository.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
 using RagnaLib.Domain.Bases.Abstracts;
+using RagnaLib.Domain.Bases.Interfaces;
 using RagnaLib.Domain.Entities;
 using RagnaLib.Domain.Repositories;
 using RagnaLib.Infra.Data;
 
 namespace RagnaLib.Infra.Repositories
 {
-    public class MonsterRepository : RepositoryBase<Context, Monster>
+    public class MonsterRepository : RepositoryBase<IContext, Monster>
     {
-        public MonsterRepository(Context context) : base(context)
+        public MonsterRepository(IContext context) : base(context)
         {
         }
 
