af2c92d03400ebc739e0ca3a36bb8b28adf55927
berthot <matheus.berthot@gmail.com>
2021-04-10 10:58:19 -0300

    create constructor

--- RagnaLib.Application/Services/ItemService.cs
+++ RagnaLib.Application/Services/ItemService.cs
@@ -1,9 +1,15 @@
+using RagnaLib.Application.Factory;
 using RagnaLib.Domain.Services;
 
 namespace RagnaLib.Application.Services
 {
     public class ItemService : IItemService
     {
-        
+        private readonly ItemFactory _factory;
+
+        public ItemService()
+        {
+            _factory = new ItemFactory();
+        }
     }
 }
--- RagnaLib.Application/Services/MonsterService.cs
+++ RagnaLib.Application/Services/MonsterService.cs
@@ -1,9 +1,15 @@
+using RagnaLib.Application.Factory;
 using RagnaLib.Domain.Services;
 
 namespace RagnaLib.Application.Services
 {
     public class MonsterService : IMonsterService
     {
+        private readonly MonsterFactory _factory;
 
+        public MonsterService()
+        {
+            _factory = new MonsterFactory();
+        }
     }
 }
